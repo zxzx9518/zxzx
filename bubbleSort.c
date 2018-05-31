79713f5a63c108878d0451af9469193991317fac
zxzx9518 <zxzx9518@gmail.com>
2018-05-31 20:01:34 +0800

    指向非常數資料的非常數指標

---
+++ bubbleSort.c
@@ -0,0 +1,28 @@
+//sort an array of intergers using bubble sort algorithm
+	void bubbleSort(int * array,unsigned int size)
+	{
+		void swap(int *element1Ptr, int *element2Ptr);	//ptototype
+		unsigned int pass;	//pass counter
+		unsigned int j;	//camparison counter
+		
+		//loop to control passes
+		for(pass=0;pass < size -1;++pass){
+			
+			//loop to control camparison during each pass
+			for(j=0;j< size -1;++j){				
+				//swap adjacent elements if theyre out of order
+				if (array[j] >array[j+1]){
+					swap(&array[j],&array[j+1]);
+				}//end if 
+			}//end inner for
+		}//end outer for
+	}//end fuction bubbleSort
+	
+	
+	//swap values at memory locations to which element1Ptr and element2Ptr point
+	void swap(int *element1Ptr, int *element2Ptr)	
+	{
+		int hold = *element1Ptr;
+		*element1Ptr=*element2Ptr;
+	    *element2Ptr=hold;
+	}//end fuction swap
---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void bubbleSort(int *  array, unsigned int size);	//ptototype
+
+int main(void) 
+{
+	//initialize array a
+	int a[SIZE]={2,0,6,8,4,9,5,1,7,3};
+	
+	unsigned int i;	//counter
+	
+	puts("Data items in original order");
+	
+	//loop through array a
+	for(i=0;i<SIZE;++i){
+		printf("%4d",a[i]);
+	}	//end for
+	
+	bubbleSort(a,SIZE);	//sort the array
+	
+	puts("\nData items in ascending order ");
+	
+	//loop through array a
+	for (i=0;i<SIZE;++i){
+		printf("%4d",a[i]);
+	}	//end for
+	
+	puts("");
+}	//end main
